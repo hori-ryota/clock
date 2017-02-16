fba59852bb1d40e93d0ae5ebaaa459ed71b3181e
hori-ryota <hori_ryota@cyberagent.co.jp>
2017-02-16 22:30:20 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {
+
+  // ブラウザ(Chromium)の起動, 初期画面のロード
+  mainWindow = new BrowserWindow({width: 1024, height: 576});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
